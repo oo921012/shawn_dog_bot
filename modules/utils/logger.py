818ff4291fbaed73428eb13323ea147047c89be6
oo921012 <oo921012@gmail.com>
2025-10-22 03:31:09 +0800

    Delete modules/utils directory

--- modules/utils/logger.py
+++
@@ -1,7 +0,0 @@
-import datetime, os
-
-def log(text):
-    if not os.path.exists("logs"):
-        os.makedirs("logs")
-    with open("logs/bot.log", "a", encoding="utf-8") as f:
-        f.write(f"[{datetime.datetime.now()}] {text}\n")
