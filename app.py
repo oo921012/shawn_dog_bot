8ea31f02089c10e93a2571afb7c93b7e2b22c9f2
oo921012 <oo921012@gmail.com>
2025-10-23 00:21:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -62,8 +62,9 @@ def left(event):
     protect.member_left(event, line_bot_api)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=10000)
     
+
 
 
 
